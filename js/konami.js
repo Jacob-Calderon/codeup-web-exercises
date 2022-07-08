6b814b732bb5c633d2ff0a3848e4ce128f4b86ce
jacob Calderon <Jacob.a.calderon@gmail.com>
2022-07-08 12:21:20 -0500

    saving

---
+++ js/konami.js
@@ -0,0 +1,13 @@
+const pressed = [];
+ const secretcode = 'qwerty'
+
+    document.addEventListener("keyup", (e) => {
+
+        // pressed.push(e.key);
+        //     pressed.splice(- secretcode.length - 1, pressed.length - secretcode.length);
+        //         console.log(pressed);
+
+        console.log(pressed.join(""))
+        // const allowedKeys = { "q", "w" ,"e","r","t","y" };
+})
+
---
+++ js/parks_national.js
@@ -0,0 +1,1 @@
+// document.body.style.backgroundImage = "url(rangerpark.jpeg)";
