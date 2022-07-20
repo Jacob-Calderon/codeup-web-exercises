6a1dcf8ddc29e344a67d4eb24d23f6e727779587
jacob Calderon <Jacob.a.calderon@gmail.com>
2022-07-20 14:33:40 -0500

    saving 1433

--- js/promises.js
+++ js/promises.js
@@ -1,12 +1,24 @@
-fetch(`https://api.github.com/users/jacob-calderon/events`, {headers: {'Authorization': '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'}})
 
-Promise.resolve('one').then((one) => {
-    console.log(one);
-    return 'two';
-}).then((two) => {
-    console.log(two);
-    return 'three';
-}).then((three) => {
-    console.log(three);
-});
-``
+var userInput = document.getElementById("user-input")
+var button= document.getElementById("brick-btn")
+
+// fetch(`https://api.github.com/users/jacob-calderon/events`, {headers: {'Authorization': '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'}})
+
+
+function action() {
+    function getGithubUsernames() {
+        return fetch(`https://api.github.com/users/${userInput.value}/events/public`, {headers: {'authorization': PROMISES_API_KEY}})
+            .then(response => response.json())
+    }
+
+// later on...
+
+    getGithubUsernames().then(users => {
+        // users.forEach( userObj => {
+        // do something with each username
+        console.log(users);
+//     });
+    }).catch(error => console.error(error));
+}
+button.addEventListener("click", action)
+    
